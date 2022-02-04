8435548bc940a51eaca998deddb42ff4f832d577
User <abdurasilov99@gmail.com>
2022-02-04 02:52:41 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Uchburchtuk triangle=new Uchburchtuk();
+        triangle.a=6;
+        triangle.b=7;
+        triangle.c=5;
+        triangle.aRea(triangle.a,triangle.b,triangle.c);
+    }
+}
---
+++ src/com/company/Uchburchtuk.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Uchburchtuk {
+    int a;
+    int b;
+    int c;
+
+    void aRea(int a, int b, int c) {
+        int d = (a + b + c) / 2;
+        System.out.println(Math.sqrt(d * (d - a) * (d - b) * (d - c)));
+
+    }
+}
